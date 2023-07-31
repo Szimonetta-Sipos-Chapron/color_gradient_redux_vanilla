f764a31965c8458f36bc79eabac44d20fc140e61
Szimonetta Sipos Chapron <siposszimonetta@gmail.com>
2023-07-31 20:41:43 +0200

    projet fini

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
-// == Imports
 import { generateSpanColor } from './utils/color';
 
-// juste en précisant le nom du dossier, il va aller chercher l'import dans index.js
 import store from './store';
 
 import './styles/index.scss';
@@ -12,14 +10,8 @@ import {
   CHANGE_DIRECTION,
 } from './store/reducers/gradientReducer';
 
-// == State
 console.log(store.getState());
 
-// DISPATCH = demander au store de faire une action
-// store.dispatch({ type: 'CHANGE_FIRST_COLOR' });
-// console.log(store.getState());
-
-// == Rendu dans le DOM
 function renderNbColors() {
   const { nbColors } = store.getState().gradient;
 
@@ -46,7 +38,6 @@ function renderColors() {
   document.querySelector('.colors').innerHTML = result;
 }
 
-// == Initialisation
 renderNbColors();
 renderGradient();
 renderColors();
@@ -57,52 +48,50 @@ store.subscribe(() => {
   renderColors();
 });
 
-// == Controls
+
 document.getElementById('randAll').addEventListener('click', () => {
-  // debug
+  
   console.log('Random all colors');
-  // data
+
   store.dispatch(CHANGE_ALL_COLORS());
 });
 
 document.getElementById('randFirst').addEventListener('click', () => {
-  // data
 
-  // on veut que le click du bouton déclenche une action de notre store
   store.dispatch(CHANGE_FIRST_COLOR());
 });
 
 document.getElementById('randLast').addEventListener('click', () => {
-  // data
+
   store.dispatch(CHANGE_LAST_COLOR());
 });
 
 document.getElementById('toLeft').addEventListener('click', () => {
-  // data
+
   store.dispatch(CHANGE_DIRECTION('270deg'));
 });
 
 document.getElementById('toRight').addEventListener('click', () => {
-  // data
+
   store.dispatch(CHANGE_DIRECTION('90deg'));
 });
 
 document.getElementById('to45').addEventListener('click', () => {
-  // data
+
   store.dispatch(CHANGE_DIRECTION('45deg'));
 });
 
 document.getElementById('to135').addEventListener('click', () => {
-  // data
+
   store.dispatch(CHANGE_DIRECTION('135deg'));
 });
 
 document.getElementById('to225').addEventListener('click', () => {
-  // data
+
   store.dispatch(CHANGE_DIRECTION('225deg'));
 });
 
 document.getElementById('to315').addEventListener('click', () => {
-  // data
+
   store.dispatch(CHANGE_DIRECTION('315deg'));
 });
--- src/store/reducers/gradientReducer.js
+++ src/store/reducers/gradientReducer.js
@@ -27,25 +27,6 @@ export default function gradientReducer(state = initialState, action = {type : '
             return state;
         }
         
-        
-        /*case "ORIENT_LEFT" : {
-            return {...state, direction : "270deg"}
-        }
-        case "ORIENT_RIGHT" : {
-            return {...state, direction : "90deg"}
-        }
-        case "ORIENT_45" : {
-            return {...state, direction : "45deg"}
-        }
-        case "ORIENT_135" : {
-            return {...state, direction : "135deg"}
-        }
-        case "ORIENT_225" : {
-            return {...state, direction : "225deg"}
-        }
-        case "ORIENT_315" : {
-            return {...state, direction : "315deg"}
-        }*/
         default :
             return state;
     }
@@ -56,9 +37,3 @@ export const CHANGE_LAST_COLOR = () => {return {type : "CHANGE_LAST_COLOR" }}
 export const CHANGE_ALL_COLORS = () => {return {type : "CHANGE_ALL_COLORS" }}
 export const CHANGE_DIRECTION = (direction) => {return {type : "CHANGE_DIRECTION", payload: direction }}
 
-/*export const ORIENT_LEFT = () => {return {type : "ORIENT_LEFT" }}
-export const ORIENT_RIGHT = () => {return {type : "ORIENT_RIGHT" }}
-export const ORIENT_45 = () => {return {type : "ORIENT_45" }}
-export const ORIENT_135 = () => {return {type : "ORIENT_135" }}
-export const ORIENT_225 = () => {return {type : "ORIENT_225" }}
-export const ORIENT_315 = () => {return {type : "ORIENT_315" }}*/
